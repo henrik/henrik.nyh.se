cceb33d705e82f61608d772ecc32f01620b47b00
Henrik Nyh <henrik@nyh.se>
2025-02-28 23:31:36 +0000

    Remove weeknotes
    Don't feel like having them linkable anymore.

--- config.rb
+++ config.rb
@@ -11,7 +11,6 @@ activate :asset_hash
 page "/*.xml", layout: false
 page "/*.json", layout: false
 page "/*.txt", layout: false
-page "/weeknotes/*", layout: "weeknote"
 
 # Methods defined in the helpers block are available in templates.
 # https://middlemanapp.com/basics/helper-methods/
@@ -33,20 +32,6 @@ helpers do
     extra_classes = (given_path_root == current_path_root) ? "links__link--current" : ""
 
     list_link_to(text, path, extra_classes:)
-  end
-
-  def week_of(year, week_number)
-    week_start = Date.commercial(year, week_number, 1)  # 1 is Monday.
-    week_start.strftime("%B #{week_start.day.ordinalize}")
-  end
-
-  def year_and_week_of_page(page)
-    basename = page.path.delete_prefix("weeknotes/").delete_suffix(".html")
-    year, week = basename.match(/\A(\d{4})-w(\d+)\z/).captures.map(&:to_i)
-  end
-
-  def weeknote_separator
-    content_tag(:div, class: "weeknote-separator") { "✧" * 3 }
   end
 
   def site_name = "Henrik Nyh"
